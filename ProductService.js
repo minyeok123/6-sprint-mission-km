09027acd19c49ddcb51cd129bebd19dd41a03732
minyeok <rltkeh150@naver.com>
2025-10-15 13:21:58 +0900

    update ProductService.js new function.POST,PATCH,DLETE .

--- ProductService.js
+++ ProductService.js
@@ -11,3 +11,56 @@ export async function getProductList(page, pageSize, keyword) {
 
   return data;
 }
+//-----------------------------------------------------------------------
+export async function getProduct(id) {
+  let res = await fetch(
+    `https://panda-market-api-crud.vercel.app/products/${id}`
+  );
+  let data = await res.json();
+  return data;
+}
+//------------------------------------------------------------------------
+export async function creatProduct(productData) {
+  let res = await fetch("https://panda-market-api-crud.vercel.app/products", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(productData),
+  });
+  let data = await res.json();
+  return data;
+}
+const NewProductData = {
+  name: "RTX9080",
+  description: "Graphic Card",
+  price: 600000000,
+  tags: "전자제품",
+  images: ["https://example.com/..."],
+};
+//------------------------------------------------------------------------
+export async function patchPrduct(id, productData) {
+  let res = await fetch(
+    `https://panda-market-api-crud.vercel.app/products/${id}`,
+    {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(productData),
+    }
+  );
+
+  let data = await res.json();
+  return data;
+}
+//---------------------------------------------------------------------
+export async function deleteProduct(id) {
+  let res = await fetch(
+    `https://panda-market-api-crud.vercel.app/products/${id}`,
+    {
+      method: "DELETE",
+    }
+  );
+  let data = await res.json();
+  return data;
+}
+//=======================================================================
