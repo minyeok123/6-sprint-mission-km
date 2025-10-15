117a2d27e2a51a45ccb2ed01ad7aff7e416c4667
minyeok <rltkeh150@naver.com>
2025-10-15 10:50:16 +0900

    new function.favorite in class PRODUCT

--- ProductClass.js
+++ ProductClass.js
@@ -5,5 +5,13 @@ export class PRODUCT {
     this.price = price;
     this.tags = tags;
     this.images = images;
+    this.찜꽁 = "💖x 0"; //찜꽁
+  }
+  favorite() {
+    console.log(`${this.name}을(를) 찜 하였습니다.💖`);
+
+    let num = Number(this.찜꽁[3]);
+    num += 1;
+    this.찜꽁 = this.찜꽁.slice(0, 3) + num;
   }
 }
