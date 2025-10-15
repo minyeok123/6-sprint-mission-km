9c75e5d28af6ae42e53450871c855c6203f6caf8
minyeok <rltkeh150@naver.com>
2025-10-15 10:40:50 +0900

    creat ProductClass.js file , update class PRODUCT

---
+++ ProductClass.js
@@ -0,0 +1,9 @@
+export class PRODUCT {
+  constructor(name, description, price, tags, images) {
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.tags = tags;
+    this.images = images;
+  }
+}
