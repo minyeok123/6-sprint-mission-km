852668eec715044c4faf92112632b1c514eb7a1e
minyeok <rltkeh150@naver.com>
2025-12-03 18:29:28 +0900

    structs ts 변환 완료

--- src/structs/userStruct.ts
+++ src/structs/userStruct.ts
@@ -1,9 +1,11 @@
 import * as s from 'superstruct';
-import isUuid from 'is-uuid';
 import isEmail from 'is-email';
 
+const emailValidator = (value: unknown): boolean => {
+  return typeof value === 'string' && isEmail(value);
+};
 export const CreateUser = s.object({
-  email: s.define('Email', isEmail), //s.define(<> ,...) <> =검사에서 실패했을 때 에러메시지에 사용
+  email: s.define('Email', emailValidator), //s.define(<> ,...) <> =검사에서 실패했을 때 에러메시지에 사용
   password: s.size(s.string(), 8, 20),
   nickname: s.size(s.string(), 2, 10),
   name: s.size(s.string(), 1, 10),
@@ -13,7 +15,7 @@ export const CreateUser = s.object({
 export const PatchUser = s.partial(s.omit(CreateUser, ['password']));
 
 export const LoginUser = s.object({
-  email: s.define('Email', isEmail),
+  email: s.define('Email', emailValidator),
   password: s.size(s.string(), 8, 20),
 });
 
